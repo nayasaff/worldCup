52f7f5aad3c64c3f11bb5be9077fe94260eb06c8
unknown <nayasafwandaba@gmail.com>
2023-01-07 12:19:33 +0200

    finale

---
+++ index.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const cors=require("cors");
+const apiRoutes = require('./routes/api');
+const { startKafkaProducer } = require('./connectors/kafka');
+const rateLimit = require('express-rate-limit') ;
+
+// Config setup to parse JSON payloads from HTTP POST request body
+
+
+
+// Apply the rate limiting middleware to all requests
+
+const limiter = rateLimit({
+	windowMs: 2 * 60 * 1000, // 2 minutes
+	max: 4, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  message:
+		'Too many purchases are happening, please try again in a minute',
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
+app.use(limiter)
+
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", value="*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods","POST,PATCH,GET,DELETE");
+  next();
+});
+
+app.use(function (req, res, next) {
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+app.use(cors({ origin : '*'}));
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+
+// Register the api routes
+apiRoutes(app);
+
+// If request doesn't match any of the above routes then return 404
+
+app.get("/api", (req, res)=>{
+  res.send("succes")
+})
+
+app.use((req, res, next) => {
+  return res.status(404).send("error wrong request");
+});
+
+
+// Create HTTP Server and Listen for Requests
+
+
+
+app.listen(7789, async (req, res) => {
+  // Start Kafka Producer
+  await startKafkaProducer();
+  console.log("client listening on")
+});
---
+++ routes/api.js
@@ -0,0 +1,96 @@
+require('dotenv').config();
+const axios = require('axios');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const { v4 } = require('uuid');
+const db = require('../connectors/postgres');
+const { sendKafkaMessage } = require('../connectors/kafka');
+const { validateTicketReservationDto } = require('../validation/reservation');
+const messagesType = require('../constants/messages');
+
+module.exports = (app) => {
+  // HTTP endpoint to test health performance of service
+  app.get('/api/v1/health', async (req, res) => {
+    return res.send('Service Health');
+  });
+
+  // HTTP endpoint to create new user
+  app.post('/api/v1/reservation', async (req, res) => {
+    try {
+      // validate payload before proceeding with reservations
+      const validationError = validateTicketReservationDto(req.body);
+      if (validationError) {
+        console.log(validationError.message)
+        return res.status(403).send(validationError.message);
+      }
+      // Send message indicating ticket is pending checkout
+      // so shop consumers can process message and call
+      // sp-shop-api to decrement available ticket count
+      await sendKafkaMessage(messagesType.TICKET_PENDING, {
+        meta: { action: messagesType.TICKET_PENDING},
+        body: { 
+          matchNumber: req.body.matchNumber,
+          tickets: req.body.tickets,
+        }
+      });
+      
+      // Perform Stripe Payment Flow
+     
+
+      try {
+        const token = await stripe.tokens.create({
+          card: {
+            number: req.body.card.number,
+            exp_month: req.body.card.expirationMonth,
+            exp_year: req.body.card.expirationYear,
+            cvc: req.body.card.cvc,
+          },
+        });
+        await stripe.charges.create({
+          amount: req.body.tickets.quantity * req.body.tickets.price,
+          currency: 'usd',
+          source: token.id,
+          description: 'FIFA World Cup Ticket Reservation',
+        });
+        
+        const response = await axios.post("https://payment-api-sable.vercel.app/payment", {
+             id : v4(),
+            amount: req.body.tickets.quantity * req.body.tickets.price
+            
+        })
+        await sendKafkaMessage(messagesType.TICKET_RESERVED, {
+          meta: { action: messagesType.TICKET_RESERVED},
+          body: { 
+            matchNumber: req.body.matchNumber,
+            tickets: req.body.tickets,
+          }
+        });
+      } catch (stripeError) {
+        // Send cancellation message indicating ticket sale failed
+
+        await sendKafkaMessage(messagesType.TICKET_CANCELLED, {
+          meta: { action: messagesType.TICKET_CANCELLED},
+          body: { 
+            matchNumber: req.body.matchNumber,
+            tickets: req.body.tickets,
+          }
+        });
+        return res.status(400).send(`could not process payment: ${stripeError.message}`);
+      }
+      
+      // Persist ticket sale in database with a generated reference id so user can lookup ticket
+      const ticketReservation = { id: v4(), ...req.body };
+      await axios.post("https://shop-api-2-de325v29m-nayasaff.vercel.app/api/reservation", ticketReservation)
+  
+      // Return success response to client
+      return res.json({
+        message: 'Ticket Purchase Successful',
+        ...ticketReservation,
+      });
+    } catch (e) {
+      console.log(e.message)
+      return res.status(400).send(e.message);
+    }
+
+    
+  });
+};
